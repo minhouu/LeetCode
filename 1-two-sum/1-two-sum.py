926ba401fdd93c910af2ae14178f1d3732ef8a7a
Minho Choi <109497339+minhouu@users.noreply.github.com>
2022-07-20 18:57:27 +0900

    Time: 124 ms (42.50%), Space: 15.2 MB (50.46%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         dic={}
-        for index, number in enumerate(nums) :
-            s_num=target-number
-            if s_num not in dic :
-                dic[number]=index
-            else :
-                return [index, dic[s_num]]
-                
+        for i in range(len(nums)) :
+            dic[nums[i]] = i
+        for i in range(len(nums)) :
+            complement = target - nums[i]
+            if complement in dic and dic[complement] != i:
+                return [i, dic[complement]]
